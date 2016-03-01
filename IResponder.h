00f84d9eb4d54ce97b51d35d546167442545d8e4
Paul Wightmore <paul.wightmore@virginmedia.com>
2016-03-01 12:58:58 +0000

    Added files via upload
    Added the idea of an emitting mode, from which a responder extends

---
+++ IResponder.h
@@ -0,0 +1,20 @@
+/* IResponder interface
+ * Paul Wightmore 2015
+ */
+#ifndef _LLAPDISPATCHER_IRESPONDER_h
+#define _LLAPDISPATCHER_IRESPONDER_h
+
+namespace LLAPDispatcher {
+
+#include "llapdispatcher\INode.h"
+
+class IPayload;
+
+//! An emitting node that will respond to a query
+class IResponder : IEmitter 
+{
+};
+
+} // namespace LLAPDispatcher
+
+#endif // _LLAPDISPATCHER_IRESPONDER_h
